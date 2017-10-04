a84ebbca696624169c222ae68f512badac500b71
Martin <id15msd@cs.umu.se>
2017-10-04 16:29:04 +0200

    readFile function

--- ImageRead.py
+++ ImageRead.py
@@ -1,9 +1,11 @@
 """
 Fundamentals of Artificial Inteligence (5DV121)
-Martin Sjölund
-Fredrik Östlund
+Martin Sjolund
+Fredrik Ostlund
 2017-10-04
 """
+
+from collections import namedtuple
 
 class ImageRead:
 
@@ -12,3 +14,22 @@ class ImageRead:
 
 
     def readImage(self, file):
+        Image = namedtuple('Image', 'values, mood')
+
+        w,h = 20,20
+        imageArray = [[0 for x in range(w)] for y in range(h)]
+
+        image_file = open(file, 'r')
+
+        i = 0
+        image_file.readline(5)
+        for line in image_file:
+            valueArray = image_file.read().split()
+
+            for j in range(20):
+                imageArray[i][j] = valueArray[j]
+            i += 1
+            Image.values = valueArray
+
+
+        file.close()
