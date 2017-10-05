293a5aa5d844d263a80e429d72bcf30b33dd3c5f
Martin <id15msd@cs.umu.se>
2017-10-05 12:47:09 +0200

    file read improvement TODO: implement the next() method

--- ImageRead.py
+++ ImageRead.py
@@ -4,7 +4,7 @@ Martin Sjolund
 Fredrik Ostlund
 2017-10-04
 """
-
+import sys
 from collections import namedtuple
 
 class ImageRead:
@@ -14,22 +14,28 @@ class ImageRead:
 
 
     def readImage(self, file):
-        Image = namedtuple('Image', 'values, mood')
+        Image = namedtuple('Face', 'name, values, mood')
 
         w,h = 20,20
         imageArray = [[0 for x in range(w)] for y in range(h)]
 
+        """https://stackoverflow.com/questions/9578580/skip-first-couple-of-lines-while-reading-lines-in-python-file"""
         image_file = open(file, 'r')
+        image_file = image_file.readlines()[6:]
 
         i = 0
-        image_file.readline(5)
         for line in image_file:
-            valueArray = image_file.read().split()
+            Image.name = line
+            print(Image.name)
+            next(image_file)
+            row = line.split()
+            print row
 
             for j in range(20):
-                imageArray[i][j] = valueArray[j]
+                imageArray[i][j] = row[j]
             i += 1
-            Image.values = valueArray
-
+            Image.values = imageArray
+            next(image_file)
 
         file.close()
+        return Image
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,7 +1,14 @@
 """
 Fundamentals of Artificial Inteligence (5DV121)
-Martin Sjölund
-Fredrik Östlund
+Martin Sjolund
+Fredrik Ostlund
 2017-10-04
 """
+from ImageRead import ImageRead
+
+if __name__ == '__main__':
+    imageRead = ImageRead()
+    training = imageRead.readImage('training.txt')
+
+    print training.name[1]
 
