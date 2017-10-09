ccb8ae8e372a550f185243e9f4bd799eca37a399
Martin <id15msd@cs.umu.se>
2017-10-09 11:19:26 +0200

    Added comments

--- ImageRead.py
+++ ImageRead.py
@@ -4,20 +4,30 @@ Martin Sjolund
 Fredrik Ostlund
 2017-10-04
 """
-import sys
 from collections import namedtuple
 
 class ImageRead:
 
+    """
+    Constructs a new "ImageRead" object
+    :return: returns nothing
+
+    """
     def __init__(self):
         pass
 
-
+    """
+    Opens a textfile and reads the lines to an array
+    :param file: The text file to read from
+    :returns: an array for the Neural Network to train from
+    """
     def readImage(self, file):
+        """namedtuple to save images in, aswell as arrays to keep data"""
         Image = namedtuple('Face', 'name values mood')
         valueArray = []
         trainingArray = []
 
+        """opens file to read"""
         image_file = open(file, 'r')
 
         try:
@@ -25,7 +35,7 @@ class ImageRead:
                 if line[0] == "#" or not line:
                     continue
 
-
+                """saves the image name and the 20 lines of image values"""
                 for x in range(21):
                     if x == 0:
                         Image.name = line
