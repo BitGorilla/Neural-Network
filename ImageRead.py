c2da2d67200630df5bda725568ffecf1be976556
Martin <id15msd@cs.umu.se>
2017-10-11 10:50:10 +0200

    Removed Table with it's , fixed insertion and lookup with Dict instead

--- ImageRead.py
+++ ImageRead.py
@@ -4,9 +4,6 @@ Martin Sjolund
 Fredrik Ostlund
 2017-10-04
 """
-from collections import namedtuple
-from TableList import TableList
-
 class ImageRead:
 
     """
@@ -23,7 +20,7 @@ class ImageRead:
     """
     def readImage(self, file):
         """Table to have the name as key, and the array as value"""
-        imageTable = TableList()
+        imageDict = {}
         string = "empty"
         valueArray = []
 
@@ -36,8 +33,8 @@ class ImageRead:
                 if line[0] == "#" or not line.strip():
                     print "comment or empty line"
                     if i > 0:
-                        print "i>0, insert to table"
-                        imageTable.insert(string, valueArray)
+                        print "i>0, insert to dict"
+                        imageDict[string] = valueArray
                         print "insert complete"
                     i = 0
                     continue
@@ -45,6 +42,7 @@ class ImageRead:
                 """saves the image name and the 20 lines of image values"""
                 if i is 0:
                     string = line
+                    string = string[:-2]
                     print "Name is", string
                     i += 1
                 else:
@@ -57,4 +55,5 @@ class ImageRead:
             pass
 
         image_file.close()
-        return imageTable
+
+        return imageDict
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -5,11 +5,9 @@ Fredrik Ostlund
 2017-10-04
 """
 from ImageRead import ImageRead
-from TableList import TableList
 
 if __name__ == '__main__':
-    training = TableList
     imageRead = ImageRead()
     training = imageRead.readImage('training.txt')
 
-    print training.lookup("Image1")
+    print training.get('Image1')
