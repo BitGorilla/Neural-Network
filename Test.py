21b99f32f91c994e576fa47fed70f50f8b9eb954
Fredrik Östlund <id15fod@cs.umu.se>
2017-10-16 11:57:38 +0200

    Now guessing on different moods

--- Test.py
+++ Test.py
@@ -21,10 +21,11 @@ class Test:
             happysum = 0
             angrysum = 0
             mischievoussum = 0
+            string = "Image"+str(x+201)
 
             for y in range(20):
                 for z in range(20):
-                    greyscale = int(images.get("Image"+str(x+1))[y][z])
+                    greyscale = self.normalize(int(images.get(string)[y][z]))
 
                     sadsum += weights.get("sad"+str(y)+str(z)) * greyscale
                     happysum += weights.get("happy"+str(y)+str(z)) * greyscale
@@ -36,8 +37,15 @@ class Test:
             angryvote = self.activation(angrysum)
             mischievousvote = self.activation(mischievoussum)
 
+            """print sadvote
+            print happyvote
+            print angryvote
+            print mischievousvote"""
+
             answer = self.vote(happyvote, sadvote, mischievousvote, angryvote)
-            facitanswer = facit.get("Image"+str(x+1))
+            facitanswer = facit.get("Image"+str(x+201))
+            print "Guess: %d" %answer
+            print "Correct: %s" %facitanswer
 
             if int(facitanswer) == answer:
                 correctAnswers += 1
@@ -45,7 +53,11 @@ class Test:
         print "%d" %correctAnswers
 
     def activation(self, sum):
+        #print sum
         return math.tanh(sum)
+
+    def normalize(self, x):
+        return x/31
 
     def vote(self, a,b,c,d):
         vote = 1
